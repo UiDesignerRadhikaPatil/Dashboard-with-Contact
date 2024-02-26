16aafec312df464688f83b8b253e284052a7d0e4
Radhika Patil /Jadhav <rahikajadhv227@gmail.com>
2024-02-26 10:35:49 +0530

    first commit

---
+++ src/component/Inputbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './inputbox.css';
+
+
+function Inputbox  ()  {
+  return (
+    <div className="inputbox-container col-12">
+       <div className='inputfield'>
+        <input type='text' />
+       </div>
+    </div>
+  )
+}
+
+export default Inputbox
